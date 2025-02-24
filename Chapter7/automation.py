4ec7eb1db5301e71f3ec6ebe55610266818e931b
prasannabe2004 <prasannabe2004@gmail.com>
2025-02-24 13:14:30 +0530

    Adding notes, chapter7 and chapter 8

---
+++ Chapter7/automation.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service  import Service
+from selenium.webdriver.chrome.options  import Options
+from selenium.webdriver.common.keys  import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manage.chrome import ChromeDriverManager
+
+chrome_options = Options()
+
+chrome_options.add_argument('--log-level=3')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+chrome_options.add_experimental_option("detach", True)
+
+service = Service(ChromeDriverManager().install())
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+driver.get("https://www.python.org/")
+
+search_bar = driver.find_element(By.NAME, "q")
+search_bar.clear()
+search_bar.send_keys("Python Documentation")
+search_bar.send_keys(Keys.RETURN)
+
+try:
+    search_result = driver.find_element(By.PARTIAL_LINK_TEXT,"Documentation by Version")
+    search_result.click()
+except Exception as e:
+    print("Coud not find the specific link")
+
+try:
+    search_result = driver.find_element(By.PARTIAL_LINK_TEXT,"3.11.9")
+    search_result.click()
+except Exception as e:
+    print("Coud not find the specific link")
+
+input("Press Enter to close the browser...")
+
+driver.quit()
+
---
+++ Chapter8/monitoringDevices.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectionHandler
+import psutil
+import time
+
+def gather_processing_metrics(device):
+    """ Monitoring CPU and Memory utilization """
+    connection = ConnectionHandler(**device)
+    cpu_out = connection.send_command('show processes cpu')
+    mem_out = connection.send_command('show processes memory')
+    return cpu_out, mem_out
+
+def gather_config_data(device):
+    """ Collect device configuation info """
+    connection = ConnectionHandler(**device)
+    running_config = connection.send_command('show running-config')
+    interfaces = connection.send_command('show interfaces')
+    return running_config, interfaces
+
+def gather_time_metrics(device):
+    """ Monitoring time-based metrics like uptime and response time """
+    connection = ConnectionHandler(**device)
+    uptime = connection.send_command('show version | include uptime')
+    response_times = []
+    for _ in range(5):
+        start_time = time.time()
+        connection.send_command('show clock')
+        end_time = time.time()
+        response_times.append(end_time - start_time)
+    return uptime, sum(response_times)/len(response_times)
+
