157c94a2cc3fe247befd5376b005e02a93c6ba53
Prasanna Padmanaban Mohanasundaram <ppadmoh@amazon.com>
2025-02-24 16:32:50 +0530

    Added notes for all chapters and ran successfully in native linux ubuntu 24

--- Chapter8/monitoringDevices.py
+++ Chapter8/monitoringDevices.py
@@ -1,24 +1,24 @@
-from netmiko import ConnectionHandler
+from netmiko import ConnectHandler
 import psutil
 import time
 
 def gather_processing_metrics(device):
     """ Monitoring CPU and Memory utilization """
-    connection = ConnectionHandler(**device)
+    connection = ConnectHandler(**device)
     cpu_out = connection.send_command('show processes cpu')
     mem_out = connection.send_command('show processes memory')
     return cpu_out, mem_out
 
 def gather_config_data(device):
     """ Collect device configuation info """
-    connection = ConnectionHandler(**device)
+    connection = ConnectHandler(**device)
     running_config = connection.send_command('show running-config')
     interfaces = connection.send_command('show interfaces')
     return running_config, interfaces
 
 def gather_time_metrics(device):
     """ Monitoring time-based metrics like uptime and response time """
-    connection = ConnectionHandler(**device)
+    connection = ConnectHandler(**device)
     uptime = connection.send_command('show version | include uptime')
     response_times = []
     for _ in range(5):
