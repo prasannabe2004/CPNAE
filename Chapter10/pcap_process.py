125a4aa58af275aa2003b40bacd2b6a6d67c8f3e
Prasanna Padmanaban Mohanasundaram <ppadmoh@amazon.com>
2025-02-25 15:32:38 +0530

    reading pcap file and printing is working

--- Chapter10/pcap_process.py
+++ Chapter10/pcap_process.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 
 def find_packets_to_ip(pacp_file, target_ip):
-    packets = rndcap(pacp_file)
+    packets = rdpcap(pacp_file)
     packet_count = 0
     for packet in packets:
         if IP in packet and packet[IP].dst == target_ip:
@@ -11,7 +11,7 @@ def find_packets_to_ip(pacp_file, target_ip):
     print(f"Packets destined to {target_ip} is {packet_count}")
 
 
-pcap_file = "pcatired_traffic.pcap"
+pcap_file = "captured_traffic.pcap"
 
 find_packets_to_ip(pcap_file, "8.8.8.8")
 
